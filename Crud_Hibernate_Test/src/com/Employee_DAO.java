0ff5be6781794c0b0559c91c62ded75d0ea7db07
surade2001 <swapnilurade2001@gmail.com>
2023-06-28 13:12:27 +0530

    init

---
+++ Crud_Hibernate_Test/src/com/Employee_DAO.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Employee_DAO {
+	public static void main(String[] args) {
+		System.out.println("Insert Employee One");
+		Employee_insert.insert_data();
+		System.out.println();
+		System.out.println("Get Employee One");
+		Employee_Get.get_data1();
+		System.out.println();
+		System.out.println("Update Employee One to Employee Two");
+		Employee_Update.update1();
+		System.out.println();
+		System.out.println("Delete Employee Two");
+		Employee_Delete.delete();
+	}
+}
